b8048bd667f57647dc3da861e2dded1f3ccaff72
Fouche du Preez <fouche@limehouse.co.za>
2024-05-18 19:45:39 +0200

    init

---
+++ src/routes/fun/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let someColor = 'green'
+	const changeColor = () => {
+		if (someColor === 'blue')
+			someColor = 'red'
+		else {
+			someColor = 'blue'
+		}
+	}
+</script>
+<h2 style="color: {someColor}">Svelte &amp; SvelteKit rocks!</h2>
+<button on:click={()=>changeColor()}>Change Color</button>
