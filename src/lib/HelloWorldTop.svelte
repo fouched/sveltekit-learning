195bd12144c9562475740231dc177abed49864e0
Fouche du Preez <fouche@limehouse.co.za>
2024-05-21 21:23:59 +0200

    added state component

--- src/lib/HelloWorldTop.svelte
+++ src/lib/HelloWorldTop.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
 	export let msg = ""
+	let isTrue = true
+
+	const toggleTrue = () => {
+		isTrue = !isTrue
+	}
 </script>
 <h1 class="h1-blue">{msg}</h1>
+<hr />
+{#if isTrue}
+	<p>Is true</p>
+{:else}
+	<p>Is false</p>
+{/if}
+<hr />
+<a href="#!" class="btn btn-outline-secondary" on:click={toggleTrue}>Toggle isTrue</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
 	import HelloWorldTop from '$lib/HelloWorldTop.svelte';
-	import HelloWorldBottom from '$lib/HelloWorldBottom.svelte';
 </script>
-<HelloWorldTop msg="Hello, prop"/>
-<HelloWorldBottom msg="Hello, prop again"/>
+<HelloWorldTop msg="Hello, again!"/>
