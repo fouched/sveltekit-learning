db8c5a1eb51333db61b91c8164588b9371aea31f
Fouche du Preez <fouche@limehouse.co.za>
2024-06-07 20:23:07 +0200

    added basic form handling

---
+++ src/routes/basic-form/+page.server.ts
@@ -0,0 +1,19 @@
+
+export const actions = {
+
+	default: async ({request }) => {
+		const formData = await request.formData();
+		const firstName = formData.get('firstName');
+		const lastName = formData.get('lastName');
+		const email = formData.get('email');
+		const password = formData.get('password');
+		console.log("firstName:", firstName)
+		console.log("lastName:", lastName)
+		console.log("email:", email)
+		console.log("password:", password)
+
+		// Process the form data and perform actions
+		const returnVal = firstName === 'Fouche' || firstName === 'John';
+		return { success: returnVal};
+	}
+}
---
+++ src/routes/basic-form/+page.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	export let form;
+
+</script>
+
+<div class="col-md-6 offset-md-3">
+	{#if form}
+		{#if form?.success}
+			<div class="alert alert-success">Form saved. Enter another?</div>
+		{:else}
+			<div class="alert alert-danger">Invalid form data</div>
+		{/if}
+	{/if}
+	<h2 class="mb-3 text-center">Basic Form</h2>
+	<form method="POST">
+		<label for="firstName" class="form-label">First Name</label>
+		<input id="firstName" name="firstName" type="text" class="form-control" autocomplete="off">
+		<label for="lastName" class="form-label">Last Name</label>
+		<input id="lastName" name="lastName" type="text" class="form-control" autocomplete="off">
+		<label for="email" class="form-label">Email</label>
+		<input id="email" name="email" type="text" class="form-control" autocomplete="off">
+		<label for="password" class="form-label">Password</label>
+		<input id="password" name="password" type="password" class="form-control mb-3">
+		<input type="submit" class="btn btn-primary" value="Save" />
+	</form>
+</div>
